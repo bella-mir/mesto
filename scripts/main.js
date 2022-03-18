b3400563392fbbfbce647bc6bbb184d73a2e8b14
Bella Mironova <belkamirbella@yandex.ru>
2022-03-18 16:41:01 +0300

    first iteration

---
+++ scripts/main.js
@@ -0,0 +1,48 @@
+let popup = document.querySelector('#popup'); // объект попапа
+let edit_button = document.querySelector('#edit_button'); // кнопка редактирования
+let popup__close = document.querySelector('.popup__close'); // кнопка заркытия
+let formElement = document.querySelector('.form'); // форма
+let nameInput = formElement.querySelector('#name'); // элемент ввода имени
+let jobInput = formElement.querySelector('#occupation'); // элемент ввода профессии
+let namePage = document.querySelector('.profile__name'); // элемент имени пользователя
+let jobPage = document.querySelector('.profile__description'); // элемент описания профессии
+let like = document.querySelector('.place__like');// элемент сердечка (лайк)
+
+
+
+function openedPopup() {
+    popup.classList.add('popup__show'); 
+};
+
+function closedPopup() {
+    popup.classList.remove('popup__show');
+};
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    namePage.textContent = nameInput.value;
+    jobPage.textContent = jobInput.value;
+    closedPopup();  
+};
+
+function activeLike () {
+    like.classList.toggle('active');
+}
+
+edit_button.addEventListener('click', openedPopup);
+
+popup__close.addEventListener('click', closedPopup);
+
+formElement.addEventListener('submit', formSubmitHandler); 
+
+like.addEventListener('click', activeLike);
+
+
+
+
+
+
+
+
+
+
